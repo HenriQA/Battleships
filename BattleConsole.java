746f27b5d2d17219a115f304b8812d27d54d5e51
HenriQA <henri.william@qa.com>
2016-11-09 09:16:43 +0000

    Update BattleConsole.java

--- BattleConsole.java
+++ BattleConsole.java
@@ -54,7 +54,7 @@ public class BattleConsole {
 	
 	while(isDirection == false){
 		String direction = reader.next();
-		if (direction.equals(up) || direction.equals(down) || direction.equals(left) || direction.equals(right)){
+		if (direction.equals("up") || direction.equals("down") || direction.equals("left") || direction.equals("right")){
 			isDirection = true;
 		}
 		else{
