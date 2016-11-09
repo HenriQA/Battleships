bdce775eaa4bd0e32b5f23706d0564f35ab6fb79
HenriQA <henri.william@qa.com>
2016-11-09 15:22:24 +0000

    Update BattleConsole.java

--- BattleConsole.java
+++ BattleConsole.java
@@ -243,7 +243,14 @@ public class BattleConsole {
                 	}
                 	System.out.print("|  " + j + "\n");
         	}
-		System.out.println(" 0 1 2 3 4 5 6 7 8 9");
+		for(int j=0; j<player.length;j++){
+			if (j < 10)
+				System.out.print(" " + j);
+			else
+				System.out.print(j);
+		}
+		System.out.print("\n");
+			
 	}
 	public void showSquare(int c,int r, Square[][] playera){
         	if(playera[r][c].getIsHit()==true && playera[r][c].getIsEmpty()==true){
